d3febc5ef87dc7093a9b3b18d201563af736d7e2
James Peng <clhsstarposter@gmail.com>
2019-04-16 22:12:27 +0800

    added transparency fix

--- Scripts/main.js
+++ Scripts/main.js
@@ -10,7 +10,7 @@ window.addEventListener('scroll', function(e){
     if(pos > 900){
         loga.style.display = "none";
         logb.style.display = "initial";
-        nb.style.backgroundColor = "white";
+        nb.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
     }else{
         loga.style.display = "initial";
         logb.style.display = "none";
