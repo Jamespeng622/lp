36a60e7d3c869d603758909f804f451f1ff18c2d
James Peng <clhsstarposter@gmail.com>
2019-04-16 21:41:43 +0800

    added transformation

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,18 +1,19 @@
 window.addEventListener('scroll', function(e){
     let pos = 0;
-    var log = document.getElementById("log");
+    var loga = document.getElementById("loga");
+    var logb = document.getElementById("logb");
     var nb = document.getElementById("nb");
 
     pos = window.scrollY;
     console.log(pos.toString());
 
     if(pos > 900){
-        log.src = "../Landing Page/Images/logo_s.png";
-        log.width = "200px";
+        loga.style.display = "none";
+        logb.style.display = "initial";
         nb.style.backgroundColor = "white";
     }else{
-        log.src = "../Landing Page/Images/logo_en.png";
-        log.width = "150px";
+        loga.style.display = "initial";
+        logb.style.display = "none";
         nb.style.backgroundColor = "rgba(255, 255, 255, 0)";
     }
 });
